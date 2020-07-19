ef830a15c4c30c016fc6107fc2add9918d88db93
alejandro <33433391+yimy19@users.noreply.github.com>
2020-07-19 15:04:19 -0500

    Add files via upload
    sitio web en desarrollo

---
+++ index.php
@@ -0,0 +1,228 @@
+<?php
+include "headers.php";
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0 ">
+            <title>
+                ALCATRAZ |&nbspInicio
+            </title>
+            <link href="css/estilo.css" rel="stylesheet" type="text/css">
+                <link href="images/logo.png" rel="icon">
+                </link>
+            </link>
+        </meta>
+<!-- Start of Async Callbell Code -->
+<script>
+  window.callbellSettings = {
+    token: "9inMLL5YJFjSy1Zezrd1zPdN"
+  };
+</script>
+<script>
+  (function(){var w=window;var ic=w.callbell;if(typeof ic==="function"){ic('reattach_activator');ic('update',callbellSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Callbell=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://dash.callbell.eu/include/'+window.callbellSettings.token+'.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+</script>
+<!-- End of Async Callbell Code -->
+    </head>
+    <body>
+        <center>
+            <section class="banner">
+                <center>
+                    <img alt="logo" class="logo" src="images/logo.png">
+                    </img>
+                </center>
+                <?php echo $bienvenidad ?>
+            </section>
+        </center>
+        <nav>
+            <ul>
+                <li>
+                   <?php echo $menu ?>
+                </li>
+            </ul>
+        </nav>
+        <article class="index">
+            <section class="historia">
+                <h2 class="titulo" style="color: white">
+                    Historia
+                </h2>
+                <br>
+                    <p class="relleno1"style="color: white;">
+                        La hostería Alcatraz es una empresa dedicada a satisfacer las necesidades de recreación, hospedaje y sano esparcimiento a nuestros clientes, buscando que las personas interactúen con el medio ambiente, se acojan a las normas socioculturales y económicas .
+                        
+                    <p class="relleno2" style="color: white">
+                       En el 2020 la Hostería Alcatraz pretende ser una empresa posicionada a nivel nacional como prestadora de servicios de hospedaje, alimentación, descanso, recreación, ofreciendo servicios personalizados de primera calidad a nuestros clientes, en un ambiente familiar para disfrutar en armonía con su entorno y con el cumplimiento de la normatividad vigente.
+                    </p>
+                </br>
+            </section>
+        </article>
+           
+x
+
+
+
+
+<br>
+    <br>
+        <section class="historia2">
+            <h2 class="titulo" style="color: white">
+                Nuestro Restaurante
+            </h2>
+            <br>
+                <p class="relleno1" style="color: white">
+                    Cuenta con alimentos de mas alta calidad y se encuentra en las mejores manos de nuestros cociner@s.  
+                </p>
+            </br>
+        </section>
+    </br>
+</br>
+   
+
+    </body>
+  
+
+ <section class="historia3" style="text-align: center">
+            <h2 class="titulo">
+               NUESTROS ALIMENTOS 
+            </h2>
+            <br>
+                <p class="relleno1">
+                    DE LA MAS ALTA CALIDAD.
+                </p>
+                <br>
+                <br>
+            
+        </section>
+
+  <div class="contenedor-galeria">
+        <ul>
+            <li>
+                <div class="titulo-imagen">
+                    <a href="menu.php">Imágen 1</a>
+                </div>
+                    <img src="images/sancocho.jpg">
+            </li>
+            <li>
+                <div class="titulo-imagen">
+                    <a href="menu.php">Imágen 2</a>
+                </div>
+                <img src="images/bandejapaisa.jpg">
+            </li>
+            <li>
+                <div class="titulo-imagen">
+                  <a href="menu.php">Imágen 3</a>
+                </div>
+                <img src="images/6.png">
+            </li>
+            <li>
+                <div class="titulo-imagen">
+                  <a href="menu.php">Imágen 4</a>
+                </div>
+                <img src="images/7.png">
+            </li>  
+              <li>
+                <div class="titulo-imagen">
+                    <a href="menu.php">Imágen 2</a>
+                </div>
+                <img src="images/bandejapaisa.jpg">
+            </li>
+            <li>
+             <li>
+                <div class="titulo-imagen">
+                  <a href="menu.php">Imágen 5</a>
+                </div>
+                <img src="images/bandejapaisa.jpg">
+            </li>
+      
+      
+             <li>
+                <div class="titulo-imagen">
+                  <a href="menu.php">Imágen 6</a>
+                </div>
+                <img src="images/sopa.jpg">
+            </li>
+                  <li>
+                <div class="titulo-imagen">
+                  <a href="menu.php">Imágen 7</a>
+                </div>
+                <img src="images/sancocho.jpg">
+            </li>
+        </ul>
+    </div>
+
+
+
+
+ <section class="historia4" style="text-align: center;top:20%;">
+            <h2 class="titulo">
+                NUESTROS CERTIFICADOS
+            </h2>
+            <br>
+                <p class="relleno1">
+                    ALTA CALIDAD
+                    <figure class="" style="text-align: center">
+            <div class=""><img   width="113" height="53" 
+                src="https://hosteriaalcatraz.com.co/wp-content/uploads/2019/06/sgs-logo.png" class="" alt="" / ></div>
+        </figure>
+    
+                </p>
+
+        <figure class="" style="text-align:  center;">
+            <div class=""><img width="112" height="153" src="https://hosteriaalcatraz.com.co/wp-content/uploads/2019/07/logo_turismo_ministerioP.png" class="" alt="" /></div>
+        </figure>
+            </br>
+        </section>
+
+
+
+
+
+        
+        <div style="width:100%;position: center;">
+
+<div class="style_prevu_kit" style="background-image: url(images/4.png);"></div>
+<div class="style_prevu_kit" style="background-image: url(images/3.png);"></div>
+<div class="style_prevu_kit" style="background-image: url(images/2.png);"></div>
+<div class="style_prevu_kit" style="background-image: url(images/1.png);"></div>
+<div class="style_prevu_kit" style=" background-image: url(images/5.png);"></div>
+
+
+</div>
+   
+        <div style="width:100%;position: center;">
+
+<div class="style_prevu_kit" style="background-image: url(images/4.png);"></div>
+<div class="style_prevu_kit" style="background-image: url(images/3.png);"></div>
+<div class="style_prevu_kit" style="background-image: url(images/2.png);"></div>
+<div class="style_prevu_kit" style="background-image: url(images/1.png);"></div>
+<div class="style_prevu_kit" style=" background-image: url(images/5.png);"></div>
+
+
+</div>
+
+
+
+
+ 
+    
+
+
+
+
+
+<footer>
+      <a href="https://www.facebook.com/alcatrazhosteria" class="icono" id="icon-1"></a>
+  <a href="https://www.instagram.com/alcatrazhosteria/?hl=es" class="icono" id="icon-2"></a>
+  <a href="#" class="icono" id="icon-3"></a>
+    <p>
+        Desarrollado Por:
+    Felipe Guzmán   Yimy Ramirez
+    </p>
+    <p>
+        Correo: ALCATRAZ@gmail.com
+    Telefonos:No Tenemos Linea Telefonica
+    Gracias Por Visitar Nuestra Pagina ;)
+    </p>
+</footer>
